fa843924c8f58ab9f074dac9b20a61ccc10c7973
DAM2Alu1 <DAM2Alu1@B01-PC04.IESGUARA.LOCAL>
2022-10-18 10:15:32 +0200

    Actualizado HolaGUI 18/10/2022

---
+++ HolaGUI/src/Alumno.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author DAM2Alu1
+ */
+public class Alumno {
+    String nombre;
+    String apellidos;
+    Integer edad;
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+    
+}
